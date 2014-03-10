b539fac27c8beda61ca80c8ef1cb1bd7a5b15708
Fabrizio Marmitt <fabrizio@marmitt.com.br>
2014-03-10 13:21:22 -0300

    Exemplo de Server de Web Service que requer autenticação integrada com Windows (Active Directory, Impersonate true)

---
+++ App_Code/Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        WindowsPrincipal winPrin = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+        return "" +
+            "User.Identity.Name =                  " + Context.User.Identity.Name + "\r\n" +
+            "User.Identity.AuthenticationType =    " + Context.User.Identity.AuthenticationType + "\r\n" +
+            "User.Identity.IsAuthenticated =       " + Context.User.Identity.IsAuthenticated.ToString() + "\r\n" +
+            "WindowsPrincipal.Identity.name =      " + winPrin.Identity.Name + "\r\n" +
+            "WindowsPrincipal.AuthenticationType = " + winPrin.Identity.AuthenticationType + "\n\n";
+    }
+    
+}
